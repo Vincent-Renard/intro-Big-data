d74097abaf31fece64a79f7208359c72f7b39990
viren <vincent.renard@gmx.com>
2021-02-02 12:43:35 +0100

    + ex2

---
+++ 1-Spark/4-APriori.py
@@ -0,0 +1,13 @@
+
+
+from sys import argv
+from time import time
+if len(argv) < 2:
+    print("J'ai besoin d'un fichier d'entrée \nUsage : python3 moi.py f.txt")
+    exit()
+
+transactions=argv[1]
+
+
+def freqent_item_set_gen(transactions,minsup,verbose=False):
+    pass
